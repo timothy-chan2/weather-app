f08ec8b3db01b90316f1f5f39a2b6293be83cdeb
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2023-10-07 21:22:47 -0400

    Add semicolon to have consistent code formatting in all tsx files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 
-import '../styles/global.css'
+import '../styles/global.css';
 
 // The following lines of code is used to override the default App component
 // Added a custom layout that includes a footer on all pages
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Component {...pageProps} />
-  )
+  );
 }
 
-export default App
+export default App;
