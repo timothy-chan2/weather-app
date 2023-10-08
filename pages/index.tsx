caf597f43a20c6c5480d421eb7e94bd1f8503df5
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2023-10-07 21:20:52 -0400

    Move the styles out of the index.tsx file and into global.css

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,40 +33,6 @@ export default function Home() {
           </Link>
         </section>
       </main>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </>
   );
 }
