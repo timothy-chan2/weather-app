7bd108fe6a36d34bde0ea749c00a2450dc9d3f9e
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2023-10-07 21:23:40 -0400

    Remove a line of comment that is not applicable

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from 'next/app';
 import '../styles/global.css';
 
 // The following lines of code is used to override the default App component
-// Added a custom layout that includes a footer on all pages
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Component {...pageProps} />
