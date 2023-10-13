3ac8882b9686d37c2c3120b333e7f55e9d6445bb
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2023-10-12 20:04:11 -0400

    Define the URL that can be displayed by the Next Image component

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'openweathermap.org',
+        port: '',
+        pathname: '/img/**',
+      },
+    ],
+  },
+}
