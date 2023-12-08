8477b322eec1f10ffe595e0b4f7b6705948f7aeb
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2023-12-07 23:50:01 -0500

    Create Spacestagram page

---
+++ pages/spacestagram.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+const Spacestagram = () => {
+  return (
+    <>
+      <Head>
+        <title>Spacestagram</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      <main>
+        
+      </main>
+    </>
+  );
+}
