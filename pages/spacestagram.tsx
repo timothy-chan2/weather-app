4eef79efa5721ff630e8fa4d4cce735dadb8af56
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2023-12-08 23:59:20 -0500

    Set up to retreive data from NASA API

--- pages/spacestagram.tsx
+++ pages/spacestagram.tsx
@@ -15,4 +15,19 @@ const Spacestagram = () => {
   );
 }
 
+export const getServerSideProps = async (context) => {
+  const apiKey = process.env.NASA_API_KEY;
+  const url = `https://api.nasa.gov/planetary/apod?start_date=${startDate}&api_key=${apiKey}`;
+  const apodData = await fetch(url);
+  const apodInfo = await apodData.json();
+
+  const props = {
+    apodInfo
+  };
+
+  return {
+    props
+  };
+}
+
 export default Spacestagram;
