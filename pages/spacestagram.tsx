15e401f9cc9f1191b73966c6acea86a063b8ac81
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2023-12-14 23:45:28 -0500

    Check the API is working correctly sending the requested information to the front end

--- pages/spacestagram.tsx
+++ pages/spacestagram.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 
-const Spacestagram = () => {
+const Spacestagram = ({ apodInfo }) => {
+  console.log(apodInfo);
+  
   return (
     <>
       <Head>
@@ -17,7 +19,7 @@ const Spacestagram = () => {
 
 export const getServerSideProps = async (context) => {
   const apiKey = process.env.NASA_API_KEY;
-  const url = `https://api.nasa.gov/planetary/apod?start_date=${startDate}&api_key=${apiKey}`;
+  const url = `https://api.nasa.gov/planetary/apod?start_date=2023-12-14&api_key=${apiKey}`;
   const apodData = await fetch(url);
   const apodInfo = await apodData.json();
 
