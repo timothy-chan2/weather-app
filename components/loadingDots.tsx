e4ceee518e94804a950a38296dc28fc74c2bd994
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2023-12-18 23:59:02 -0500

    Add styles to the LoadingDots component

--- components/loadingDots.tsx
+++ components/loadingDots.tsx
@@ -1,11 +1,13 @@
+import styles from '../styles/LoadingDots.module.css';
+
 // The LoadingDots component shows three dots that grow and shrink
 // at different times while the project cards are loading
 const LoadingDots = () => {
   return (
-    <article className='loader'>
-      <span className='loader-dot' />
-      <span className='loader-dot' />
-      <span className='loader-dot' />
+    <article className={styles.loader}>
+      <span className={styles.loaderDot} />
+      <span className={styles.loaderDot} />
+      <span className={styles.loaderDot} />
     </article>
   );
 };
