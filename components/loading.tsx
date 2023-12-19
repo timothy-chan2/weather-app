cabc468b0ee8faf525f8b20e26c9287943c78f2e
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2023-12-19 00:31:34 -0500

    Add styles to the Loading component

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,14 +1,18 @@
 import Image from 'next/image';
 
+import styles from '../styles/Loading.module.css';
+
 // The Loading component shows a spinning wheel while
 // fetching the data from the NASA API
 const Loading = () => {
   return (
     <article>
       <Image
-        className='loading-image'
-        src='images/loading.png'
+        className={styles.loadingImage}
+        src='/images/loading.png'
         alt='Loading'
+        width='75'
+        height='75'
       />
       <h3>Loading...</h3>
     </article>
