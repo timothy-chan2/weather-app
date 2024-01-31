0955fbdcab23147e6dbf2cbac3212fa927d42d41
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-01-31 01:08:48 -0500

    Set up basic styles of the page

--- pages/developer.tsx
+++ pages/developer.tsx
@@ -4,6 +4,8 @@ import Head from 'next/head';
 
 import Navbar from '../components/navbar';
 
+import styles from '../styles/Developer.module.css';
+
 // The Developer component shows other projects I have done
 // with links to my GitHub page if it is not deloyed
 const Developer = () => {
@@ -13,6 +15,34 @@ const Developer = () => {
         <title>Developer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className={styles.developer}>
+        <img
+          className={styles.bgImage}
+          src='images/trunk-ring.jpg'
+          alt=''
+        />
+      
+        <div className={styles.developerContent}>
+          <header>
+            <Navbar
+              pageTitle='Developer'
+              fontColor='dark purple'
+            />
+            <h2 className={styles.developerName}>Timothy Chan</h2>
+            <a href='https://linkedin.com/in/timothychan2' target='_blank' rel='noreferrer'>
+              <img
+                src='images/In-White-34.png'
+                aria-label='LinkedIn logo'
+              />
+            </a>
+          </header>
+          <main>
+
+            <p className={styles.otherProjects}>My other projects:</p>
+
+          </main>
+        </div>
+      </div>
     </>
   );
 };
