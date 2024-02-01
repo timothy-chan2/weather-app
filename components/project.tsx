22182ff44c2ada889aba84fec892e22f4cce9eee
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-02-01 01:24:54 -0500

    Create Project component

---
+++ components/project.tsx
@@ -0,0 +1,36 @@
+import styles from '../styles/Post.module.css';
+import projectStyles from '../styles/Project.module.css';
+
+// The Project component shows an image, title, and
+// description of a project I previously worked on
+const Project = (props) => {
+  return (
+    <article className={styles.post}>
+      {props.linkUrl &&
+        <a href={props.linkUrl} target='_blank' rel='noreferrer'>
+          <img
+            src={props.imgUrl}
+            aria-label={props.title}
+            className={styles.postImg}
+            loading='lazy'
+          />
+        </a>
+      }
+      {props.videoUrl &&
+        <iframe
+          src={props.videoUrl}
+          aria-labelledby={props.title}
+          className={`${styles.postImg} ${styles.postVid}`}
+          title={props.title}
+          loading='lazy'
+        >
+          <p>Your browser does not support iframes.</p>
+        </iframe>
+      }
+      <h3 className={styles.postTitle}>{props.title}</h3>
+      <p className={`${styles.postDesc} ${projectStyles.projectDesc}`}>{props.description}</p>
+    </article>
+  );
+};
+
+export default Project;
