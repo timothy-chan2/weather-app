f3b452fd79dd27504fe5ccc7a2e1e3135b29ab9e
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-02-05 00:29:08 -0500

    Add more recent projects to the list of personal projects

--- helpers/projectData.ts
+++ helpers/projectData.ts
@@ -1,45 +1,18 @@
 const projects = [
   {
+    title: 'Spacestagram',
+    imgUrl: 'images/spacestagram.png',
+    linkUrl: 'https://spac3stagram.onrender.com',
+    desc: 'Deployed a web app to view photos from NASA’s Astronomy Picture of the Day image API with mobile first responsive design. The website can be used to increase people’s interest in science/technology. Users can select the date range as well as like and unlike any of the photos.',
+    fStack: 'React JS, JavaScript, HTML, CSS, sassy-datepicker, Axios',
+    bStack: 'Node JS, Express JS, Axios, dotenv',
+    priority: true
+  },
+  {
     title: 'Trash Panda MTL',
     imgUrl: 'images/trash-panda.gif',
     linkUrl: 'https://github.com/andres039/Upcycle-Cradle-Montreal',
-    desc: 'A web app that allows Montrealers to extend the life of discarded furniture by displaying their location on an interactive map.'
-  },
-  {
-    title: 'Technical Documentation',
-    imgUrl: 'images/tech-doc.png',
-    linkUrl: 'https://timothy-chan2.github.io/fcc-technical-documentation',
-    desc: 'A technical documentation page about JSX.'
-  },
-  {
-    title: 'Product Landing Page',
-    imgUrl: 'images/product-landing.png',
-    linkUrl: 'https://timothy-chan2.github.io/fcc-product-landing',
-    desc: 'The front end of a product landing page for a mobility scooter by Daymak, Inc. Note: The e-mail input field is not connected to a back end.'
-  },
-  {
-    title: 'Survey Form',
-    imgUrl: 'images/survey.png',
-    linkUrl: 'https://timothy-chan2.github.io/fcc-survey-form',
-    desc: 'The front end of a survey form about astronaut Chris Hadfield. Note: The input fields are not connected to a back end.'
-  },
-  {
-    title: 'Tribute Page',
-    imgUrl: 'images/tribute.png',
-    linkUrl: 'https://timothy-chan2.github.io/fcc-tribute-page',
-    desc: 'A tribute about astronaut Chris Hadfield.'
-  },
-  {
-    title: 'Jungle',
-    imgUrl: 'images/jungle-rails.png',
-    linkUrl: 'https://github.com/timothy-chan2/jungle-rails',
-    desc: 'An inherited mini e-commerce application built with Rails 4.2 that required bug fixes and additional features.'
-  },
-  {
-    title: 'Interview Scheduler',
-    imgUrl: 'images/scheduler.png',
-    linkUrl: 'https://github.com/timothy-chan2/scheduler',
-    desc: 'Interview scheduler built with React where a user can create, edit, and delete interview appointments.'
+    desc: 'A web app that allows Montrealers to extend the life of discarded furniture by displaying their location on an interactive map.',
   },
   {
     title: 'TinyApp',
@@ -48,10 +21,10 @@ const projects = [
     desc: 'A full stack web app built with Node and Express that allows users to shorten long URLs. Users can select between dark/light theme.'
   },
   {
-    title: 'Quizzle',
-    imgUrl: 'images/quizzle.png',
-    linkUrl: 'https://github.com/Sponicus/Quiz-App',
-    desc: 'A web app that lets you create and take quizzes as well as share your quiz results with friends.'
+    title: 'My Older Projects',
+    imgUrl: 'images/scheduler.png',
+    linkUrl: 'https://spac3stagram.onrender.com/developer',
+    desc: 'The older projects are found on my previous portfolio website. Please feel free to have a look.'
   }
 ];
 
