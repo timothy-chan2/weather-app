b4fd6f88d1497bff73578f041a9c4ba058ba3381
Timothy Chan <timothy.chan2@mail.mcgill.ca>
2024-04-01 23:59:23 -0400

    Add jest-dom as an import so that toBeInTheDocument works properly

--- __tests__/post.test.js
+++ __tests__/post.test.js
@@ -1,4 +1,5 @@
-import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { render, cleanup, screen } from '@testing-library/react';
 import Post from '../components/post';
 
 afterEach(cleanup);
@@ -6,5 +7,11 @@ afterEach(cleanup);
 describe('Post', () => {
   it('renders without crashing', () => {
     render(<Post />);
+  });
+
+  it('renders the default like button and the ❤️ is not visible', () => {
+    render(<Post />);
+    expect(screen.getByText('Like')).toBeInTheDocument();
+    expect(screen.queryByText('❤️')).not.toBeInTheDocument();
   });
 });
